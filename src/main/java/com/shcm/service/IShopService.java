956d98c3aa5e864a1d7903700ece1d9675f38e18
ZangYt <yt.im@outlook.com>
2023-11-12 20:50:14 +0800

    给商户查询添加缓存

--- src/main/java/com/shcm/service/IShopService.java
+++ src/main/java/com/shcm/service/IShopService.java
@@ -1,9 +1,11 @@
 package com.shcm.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.shcm.dto.Result;
 import com.shcm.entity.Shop;
 
 
 public interface IShopService extends IService<Shop> {
 
+    Result queryById(Long id);
 }
--- src/main/java/com/shcm/service/impl/ShopServiceImpl.java
+++ src/main/java/com/shcm/service/impl/ShopServiceImpl.java
@@ -1,13 +1,50 @@
 package com.shcm.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.shcm.dto.Result;
 import com.shcm.entity.Shop;
 import com.shcm.mapper.ShopMapper;
 import com.shcm.service.IShopService;
+import lombok.val;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.shcm.utils.RedisConstants.CACHE_SHOP_KEY;
 
 
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+
+        String key = CACHE_SHOP_KEY + id;
+        // 1.从redis查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        // 2.判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3.存在,直接返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        // 4.不存在,根据id查询数据库
+        Shop shop = getById(id);
+        // 5.不存在,返回错误
+        if (shop == null) {
+            return Result.fail("店铺不存在");
+        }
+        // 6.存在,写入Redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        // 7.返回
+        return Result.ok(shop);
+
+    }
 }
