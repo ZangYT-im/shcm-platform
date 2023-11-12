c9e0397698fb6b85096e10c0575d5790976d7070
ZangYt <yt.im@outlook.com>
2023-11-12 17:30:06 +0800

    发送验证码模块

--- src/main/java/com/shcm/service/IUserService.java
+++ src/main/java/com/shcm/service/IUserService.java
@@ -1,9 +1,13 @@
 package com.shcm.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.shcm.dto.Result;
 import com.shcm.entity.User;
+
+import javax.servlet.http.HttpSession;
 
 
 public interface IUserService extends IService<User> {
 
+    Result sendCode(String phone, HttpSession session);
 }
--- src/main/java/com/shcm/service/impl/UserServiceImpl.java
+++ src/main/java/com/shcm/service/impl/UserServiceImpl.java
@@ -1,13 +1,38 @@
 package com.shcm.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.shcm.dto.Result;
 import com.shcm.entity.User;
 import com.shcm.mapper.UserMapper;
 import com.shcm.service.IUserService;
+import com.shcm.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 
+
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+        // 3.符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 4.保存验证码到 session
+        //验证码默认666666,演示方便
+        session.setAttribute("code","666666");
+        // 5.发送验证码
+        log.debug("发送短信验证码成功，验证码：{}", code);
+        // 返回ok
+        return Result.ok();
+    }
 }
