789d667c726902d9324a364de74eb62e26c990e9
ZangYt <yt.im@outlook.com>
2023-11-13 23:58:50 +0800

    释放锁优化:采用lua脚本保证原子性

--- src/main/java/com/shcm/utils/SimpleRedisLock.java
+++ src/main/java/com/shcm/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.shcm.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -22,6 +25,14 @@ public class SimpleRedisLock implements ILock {
 
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        //ClassPath去resource目录下去找
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -43,23 +54,44 @@ public class SimpleRedisLock implements ILock {
         return Boolean.TRUE.equals(success);//如果是null，返回false
     }
 
+    /**
+     * 使用lua脚本释放锁，来保证原子性
+     */
     @Override
     public void unlock() {
         /**
-         * 释放锁需要优化，
+         * 释放锁需要优化
          * 极端情况下有可能加锁的和释放锁的线程id不一致
          * 所以要保证加锁和释放锁的线程是同一个
          * */
-        //获取线程标识
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        //获取锁中的标识
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        //判断标识是否一致，不一致就不释放
-        if (threadId.equals(id)){
-            //释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
-
-
+        //调用lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
+
+    /**
+     * 没有用lua脚本，只用java代码检查线程id和释放锁这两个步不能够保证原子性
+     * */
+    /*
+      @Override
+    public void unlock() {
+
+         * 释放锁需要优化，
+         * 极端情况下有可能加锁的和释放锁的线程id不一致
+         * 所以要保证加锁和释放锁的线程是同一个
+
+    //获取线程标识
+    String threadId = ID_PREFIX + Thread.currentThread().getId();
+    //获取锁中的标识
+    String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+    //判断标识是否一致，不一致就不释放
+        if (threadId.equals(id)){
+        //释放锁
+        stringRedisTemplate.delete(KEY_PREFIX + name);
+    }
+
+    * */
+
+
 }
