56e6f0b4364f360a6c52382174143922c55701b3
ZangYt <yt.im@outlook.com>
2023-11-13 16:08:57 +0800

    实现基本优惠券下单功能

--- src/main/java/com/shcm/service/IVoucherOrderService.java
+++ src/main/java/com/shcm/service/IVoucherOrderService.java
@@ -1,9 +1,11 @@
 package com.shcm.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.shcm.dto.Result;
 import com.shcm.entity.VoucherOrder;
 
 
 public interface IVoucherOrderService extends IService<VoucherOrder> {
 
+    Result seckillVoucher(Long voucherId);
 }
