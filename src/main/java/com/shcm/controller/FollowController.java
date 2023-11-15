1e0f4e3b17086b461b340a6ca0ef34e839503746
ZangYt <yt.im@outlook.com>
2023-11-15 14:44:13 +0800

    实现用户A关注B功能

--- src/main/java/com/shcm/controller/FollowController.java
+++ src/main/java/com/shcm/controller/FollowController.java
@@ -1,12 +1,29 @@
 package com.shcm.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.shcm.dto.Result;
+import com.shcm.service.IFollowService;
+import com.shcm.service.impl.FollowServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+    @Resource
+    private IFollowService followService;
 
+    //关注
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long followUserId, @PathVariable("isFollow") Boolean isFollow) {
+
+        return followService.follow(followUserId, isFollow);
+    }
+    //取消关注
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long followUserId) {
+        return followService.isFollow(followUserId);
+    }
 }
--- src/main/java/com/shcm/service/IFollowService.java
+++ src/main/java/com/shcm/service/IFollowService.java
@@ -1,9 +1,13 @@
 package com.shcm.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.shcm.dto.Result;
 import com.shcm.entity.Follow;
 
 
 public interface IFollowService extends IService<Follow> {
 
+    Result follow(Long followUserId, Boolean isFollow);
+
+    Result isFollow(Long followUserId);
 }
