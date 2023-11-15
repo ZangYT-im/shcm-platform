f3250335504a10068608b9391e7e2cd176570cbf
ZangYt <yt.im@outlook.com>
2023-11-15 20:50:20 +0800

    商铺地理位置信息用单元测试进行缓存预热

--- src/test/java/com/shcm/ShCmPlatformApplicationTest.java
+++ src/test/java/com/shcm/ShCmPlatformApplicationTest.java
@@ -11,16 +11,25 @@ import com.shcm.utils.RedisIdWorker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static com.shcm.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.shcm.utils.RedisConstants.SHOP_GEO_KEY;
 
 /**
  * <p>
@@ -34,6 +43,9 @@ import static com.shcm.utils.RedisConstants.CACHE_SHOP_KEY;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ShCmPlatformApplicationTest {
+
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
 
     @Resource
     private ShopServiceImpl shopService;
@@ -53,7 +65,7 @@ public class ShCmPlatformApplicationTest {
             for (int i = 0; i < 100; i++) {
                 long id = redisIdWorker.nextId("order");
                 System.out.println("id:" + id);
-                
+
             }
             latch.countDown();
         };
@@ -72,6 +84,7 @@ public class ShCmPlatformApplicationTest {
         Shop shop = shopService.getById(1L);
         cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
     }
+
     @Resource
     ServiceImpl<UserMapper, User> service;
 
@@ -79,9 +92,36 @@ public class ShCmPlatformApplicationTest {
     IUserService iUserService;
 
     @Test
-    public void test(){
+    public void test() {
         User user = iUserService.query().eq("phone", "13361337361").one();
         System.out.println(user.toString());
 
+    }
+
+
+    /**
+     * 预热店铺的位置信息到redis
+     * */
+    @Test
+    public void loadShopData() {
+        // 1.查询店铺信息
+        List<Shop> list = shopService.list();
+        // 2.把店铺分组,按照typeId分组,id一致的放到一个集合
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3.分批完成写入redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 3.1.获取类型id
+            Long typeId = entry.getKey();
+            String key = SHOP_GEO_KEY + typeId;
+            // 3.2.获取同类型的店铺的集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            // 3.3.写入redis GEOADD key 经度 纬度 member
+            for (Shop shop : value) {
+                locations.add(new RedisGeoCommands.GeoLocation<String>(shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
     }
 }
