72ec2fd8f1497aecc46789ca9fd03a924f47c811
ZangYt <yt.im@outlook.com>
2023-11-15 12:43:37 +0800

    单元测试

--- src/test/java/com/shcm/ShCmPlatformApplicationTest.java
+++ src/test/java/com/shcm/ShCmPlatformApplicationTest.java
@@ -1,6 +1,10 @@
 package com.shcm;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shcm.entity.Shop;
+import com.shcm.entity.User;
+import com.shcm.mapper.UserMapper;
+import com.shcm.service.IUserService;
 import com.shcm.service.impl.ShopServiceImpl;
 import com.shcm.utils.CacheClient;
 import com.shcm.utils.RedisIdWorker;
@@ -68,5 +72,16 @@ public class ShCmPlatformApplicationTest {
         Shop shop = shopService.getById(1L);
         cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
     }
+    @Resource
+    ServiceImpl<UserMapper, User> service;
 
+    @Resource
+    IUserService iUserService;
+
+    @Test
+    public void test(){
+        User user = iUserService.query().eq("phone", "13361337361").one();
+        System.out.println(user.toString());
+
+    }
 }
