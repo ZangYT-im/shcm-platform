4797a3c540d31bd1525b4a2024125bd32beaab5a
ZangYT <yt.im@outlook.com>
2024-01-14 23:04:20 +0800

    更新配置文件

--- src/main/java/com/shcm/config/RedissonConfig.java
+++ src/main/java/com/shcm/config/RedissonConfig.java
@@ -3,6 +3,7 @@ package com.shcm.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,12 +19,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
+
+    @Value("${spring.redis.host}")
+    private String redisAddress;
+
+    @Value("${spring.redis.password}")
+    private String redisPass;
+
     @Bean
     public RedissonClient redissonClient(){
         // 配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://8.130.98.117:6379")
-                .setPassword("123456");
+        config.useSingleServer().setAddress("redis://"+redisAddress+":6379")
+                .setPassword(redisPass);
         // 创建RedissonClient对象
         return Redisson.create(config);
     }
