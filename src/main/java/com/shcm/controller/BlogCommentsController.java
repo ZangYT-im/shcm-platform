02481791c3c70497849804091c3fef101345e40a
zangyt <yt.im@outlook.com>
2024-06-02 13:42:21 +0800

    评论发表，查询等功能

--- src/main/java/com/shcm/controller/BlogCommentsController.java
+++ src/main/java/com/shcm/controller/BlogCommentsController.java
@@ -1,12 +1,52 @@
 package com.shcm.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.shcm.dto.Result;
+import com.shcm.entity.BlogComments;
+import com.shcm.entity.User;
+import com.shcm.service.IBlogCommentsService;
+import com.shcm.service.IUserService;
+import com.shcm.utils.UserHolder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/blog-comments")
 public class BlogCommentsController {
 
+    @Resource
+    IBlogCommentsService blogCommentsService;
+
+    @Resource
+    IUserService userService;
+
+    /**
+     * 根据博客 id 查询对应的评论
+     * @return
+     */
+    @GetMapping("/queryCommentsListByBlogId/{blogId}")
+    public Result queryCommentsListByBlogId(@PathVariable("blogId") String blogId) {
+        QueryWrapper<BlogComments> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("blog_id", blogId);
+        List<BlogComments> blogCommentsList = blogCommentsService.getBaseMapper().selectList(queryWrapper);
+        for (BlogComments blogComments : blogCommentsList) {
+            User user = userService.getById(blogComments.getUserId());
+            blogComments.setNickName(user.getNickName());
+            blogComments.setIcon(user.getIcon());
+        }
+        System.out.println(blogCommentsList.toArray().toString());
+        return Result.ok(blogCommentsList);
+    }
+
+    @PostMapping("/saveComments")
+    public Result saveBlogCommit(@RequestBody BlogComments blogComment) {
+        blogComment.setUserId( UserHolder.getUser().getId());
+        System.out.println(blogComment);
+        boolean save = blogCommentsService.save(blogComment);
+        return Result.ok(save);
+    }
 }
--- src/main/java/com/shcm/entity/BlogComments.java
+++ src/main/java/com/shcm/entity/BlogComments.java
@@ -1,6 +1,7 @@
 package com.shcm.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -69,6 +70,12 @@ public class BlogComments implements Serializable {
      * 更新时间
      */
     private LocalDateTime updateTime;
+
+    @TableField(exist = false)
+    private String nickName;
+
+    @TableField(exist = false)
+    private String icon;
 
 
 }
